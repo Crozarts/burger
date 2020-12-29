63d2f78c9cee6f24bc3769f8c91f4129065f031b
Andrew Crosby <imarcrosby@optimum.net>
2020-12-29 13:34:13 -0500

    set up mysql connection

---
+++ config/connections.js
@@ -0,0 +1,16 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burger_db"
+  });
+}
